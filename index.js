0c8b0435b580a7309e2cadcb9f64cc5fd3bf9da6
Aminul Islam <aminulislam565688@gmail.com>
2021-04-02 02:12:29 +0600

    third commit

--- index.js
+++ index.js
@@ -4,16 +4,25 @@ require("dotenv").config();
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 7897;
+const admin = require("firebase-admin");
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // .env
 const user = process.env.DB_USER;
 const password = process.env.DB_PASS;
 const database = process.env.DB_DATABASE;
 
-//MongoDB
+//Firebase Authentication
 
+const serviceAccount = require("./PrivateKey/book-store-1856c-firebase-adminsdk-ccivo-8b8de2455b.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+//MongoDB
 const MongoClient = require("mongodb").MongoClient;
 const { ObjectId } = require("bson");
 const uri = ` mongodb+srv://${user}:${password}@cluster0.aifw0.mongodb.net/${database}?retryWrites=true&w=majority`;
@@ -23,6 +32,7 @@ const client = new MongoClient(uri, {
 });
 client.connect((err) => {
   const booksCollection = client.db(database).collection("BooksDataBase");
+  const orderCollection = client.db(database).collection("BookOrders");
 
   //adding Book To The Database
   app.post("/addBooksData", (req, res) => {
@@ -48,6 +58,39 @@ client.connect((err) => {
       .then((err, result) => {
         res.send(err.deleteCount > 0);
       });
+  });
+  //Order Add to Server
+  app.post("/addOrder", (req, res) => {
+    const orderData = req.body;
+    orderCollection.insertOne(orderData).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+  //Recent Orders and Verification Token
+  app.get("/recentOrders", (req, res) => {
+    const bearer = req.headers.authorization;
+    if (bearer && bearer.startsWith("Bearer ")) {
+      const idToken = bearer.split(" ")[1];
+      console.log({ idToken });
+      // idToken comes from the client app
+      admin
+        .auth()
+        .verifyIdToken(idToken)
+        .then((decodedToken) => {
+          const tokenEmail = decodedToken.email;
+          if (tokenEmail == req.query.email) {
+            orderCollection
+              .find({ email: req.query.email })
+              .toArray((err, document) => {
+                res.send(document);
+              });
+          }
+          console.log(uid);
+        })
+        .catch((error) => {
+          // Handle error
+        });
+    }
   });
 
   console.log("DATABASE Connected");
